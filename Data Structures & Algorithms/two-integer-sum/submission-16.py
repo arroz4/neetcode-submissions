b56cb2c94ed4d0f8297dfb3189299faf2431f012
Omar Barrera <62823474+arroz4@users.noreply.github.com>
2026-08-21 14:30:21 -0700

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ''' BRUTE FORCE'''
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                #print(i,j,"indexes")
+                #print(nums[i],nums[j],"values")
+                if nums[i] + nums[j] == target and i != j:
+                    return [i,j]
+        return 0
